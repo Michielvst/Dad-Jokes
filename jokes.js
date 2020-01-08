2f310b8e9076189a96134247beb07d48d9ea0b36
Michielvst <michiel--vs@outlook.com>
2020-01-08 16:22:55 +0100

    display joke, loader added

--- jokes.js
+++ jokes.js
@@ -1,5 +1,7 @@
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+const jokeText = document.querySelector('.jokeText');
 
 const buttonText = [
   'Ugh.',
@@ -13,8 +15,36 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
-  const response = await fetch('https://icanhazdadjoke.com/');
-  console.log(response);
+  loader.classList.remove('hidden');
+  jokeText.classList.add('hidden');
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  loader.classList.add('hidden');
+  jokeText.classList.remove('hidden');
+  return data.joke;
 }
 
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if(item === not) {
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const joke = await fetchJoke();
+  console.log(joke);
+  jokeHolder.textContent = joke;
+  jokeText.textContent = randomItemFromArray(buttonText, jokeText.textContent);
+
+}
+
+jokeButton.addEventListener('click', handleClick);
+
 fetchJoke();
+randomItemFromArray(buttonText);
